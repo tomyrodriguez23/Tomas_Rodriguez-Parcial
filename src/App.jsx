8be1b13af5e4241d4cf7bb0db018df345480921d
tomyrodriguez23 <tomyrodriguez2305@hotmail.com>
2022-11-07 17:15:43 -0300

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Card from './Card'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Hola, soy una tarjeta div</div>
+  )
+}
+
+export default Card
